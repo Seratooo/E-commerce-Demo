b04786eefe3eb54daae75e1be15d8a0ed01593a2
Seratooo <pauloserato2016@gmail.com>
2021-05-16 10:40:12 +0100

    Atualizando o cod

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import {Header} from '../componets/header/header'
 import { Item } from '../componets/items'
-
+import Link from 'next/link'
 export default function Home() {
   return (
     <div>
         <Header/>
         <Item titulo="Produtos Em destaque"/>
+        <Link href="/1?index=121">
+            <button type="button">Header</button>
+        </Link>
+
     </div>
   )
 }
